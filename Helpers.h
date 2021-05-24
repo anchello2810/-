83850598c2f8f2ae645d9337c7c7f946f3dba766
Anna Akopyan <anchello@yandex.ru>
2021-05-24 09:33:34 +0300

    Добавьте файлы проекта.

---
+++ Helpers.h
@@ -0,0 +1,5 @@
+int sum(int a, int b)
+{
+	int result = pow(a + b, 2);
+	return result;
+}
---
+++ Дз.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	std::cout << sum(3, 5) << std::endl;
+
+}
